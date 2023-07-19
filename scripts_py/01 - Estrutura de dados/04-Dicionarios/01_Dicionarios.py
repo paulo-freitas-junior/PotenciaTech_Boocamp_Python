f67c6c05b3cac485bd2892e3171c60d95faaf9ad
Paulo R. L. de F. Junior <63370929+paulo-freitas-junior@users.noreply.github.com>
2023-07-19 13:01:46 -0300

    docs: alt. comentários código

--- scripts_py/01 - Estrutura de dados/04-Dicionarios/01_Dicionarios.py
+++ scripts_py/01 - Estrutura de dados/04-Dicionarios/01_Dicionarios.py
@@ -5,18 +5,22 @@ de pares chave:valor separada por vírgulas.
 
 '''
 # Formas de criar um dicionário
+# dicionario = {'chave:valor','chave:valor'.....}
+# dicionario = dict(chave='valor', chave='valor', chave=valor, .....)
 
-pessoa = {"nome":"Guilherme", "idade":28}   # declaração 
-pessoa = dict(nome="Ze Mane", idade=88)       # via uso do dict
+pessoa = {"nome":"Paulo", "idade":46}   # declaração 
+pessoa = dict(nome="Ze Mane", idade=88) # via uso do dict
 
 
 # Add nova chave:valor ao dicionário pessoa
+# dicionario['chave'] = 'valor'
 
 pessoa["telefone'"] = "3333-1234"
 print(pessoa)
 
 
 # Acessando os dados do dicionário através das CHAVES
+# dicionario['chave']
 
 dados = {'nome':'Paulo', 'idade':46, 'telefone': '91234-1234'}
 
@@ -26,7 +30,8 @@ print(f'Idade:', dados['idade'])
 print(f'Telefone:', dados['telefone'])
 
 
-# Alterando o valor das chaves de um dicionário
+# Alterando o VALOR das chaves de um dicionário
+# dicionario['chave'] = 'valor'
 
 dados['nome'] = "Elaine"
 dados['idade'] = 43
@@ -53,7 +58,7 @@ contatos = {
     'fulana1@gmail.com': {'nome': 'Fulaninha', 'telefone': '1111-0000'},
 }
 
-# acessando a STRING e retornando o resultado da CHAVE selecionada
+# acessando a CHAVE (string) e retornando o resultado da CHAVE selecionada
 print(contatos['zemane@gmail.com']['telefone']) # '3333-2222'
 
 '''
@@ -64,11 +69,17 @@ A forma mais comum para percorer os dados de um dicionário é utilizando o coma
 
 '''
 
+contatos = {
+    'zemane@gmail.com': {'nome': 'Ze Mane', 'telefone': '3333-2222'},
+    'zezinho@gmail.com': {'nome': 'Zezinho', 'telefone': '2222-1111'},
+    'fulana1@gmail.com': {'nome': 'Fulaninha', 'telefone': '1111-0000'},
+}
+
 # Opção 1 - Não muito usada
 for chave in contatos:
     print(chave, contatos[chave])
 
-# Ppção 2 - Mais usada
+# Opção 2 - Mais usada
 for chave, valor in contatos.items():
     print(chave,valor)
     
